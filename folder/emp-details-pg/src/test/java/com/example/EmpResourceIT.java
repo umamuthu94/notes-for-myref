21d1878170cd5c567a74d9996450a19df374daff
umamuthu94 <umamuthu1994@gmail.com>
2023-08-26 06:44:07 +0530

    file1

--- folder/emp-details-pg/src/test/java/com/example/EmpResourceIT.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class EmpResourceIT extends EmpResourceTest{
-    
-}
--- folder/ticket-pg/src/test/java/com/detail/TicketResourceIT.java
+++
@@ -1,8 +0,0 @@
-package com.detail;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class TicketResourceIT extends TicketResourceTest {
-    
-}
